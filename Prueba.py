f7e95e90f41d6ab10196c6be2542a93d33231e91
LJ-CH21 <ghostcode.0101@gmail.com>
2026-05-08 21:46:32 -0500

    Diseño de bienvenida y comienzo del segundo requerimiento

--- Prueba.py
+++ Prueba.py
@@ -1,4 +1,11 @@
-class Usuario:
+from abc import ABC, abstractmethod
+
+class MostrarInfo(ABC):
+    @abstractmethod
+    def mostrar_informacion(self):
+        pass
+
+class Usuario(MostrarInfo): #Permitir a los estudiantes acceder con su correo institucional.
     def __init__(self, nombre, apellido, correo):
         self.nombre = nombre
         self.apellido = apellido
@@ -18,7 +25,10 @@ class Usuario:
             self.apellido = input("Ingrese su apellido: ")
             self.correo = input("Ingrese su correo: ")
 
+        
         self.mostrar_informacion()
+        print(f"\n----- Bienvenido/a {self.nombre} -----\n")
+        print(f"\nUTBRS                        | Mis Reseñas | {self.correo} |\n")
 
 
 nombre = input("Ingrese su nombre: ")
@@ -27,3 +37,23 @@ correo = input("Ingrese su correo: ")
 
 usuario1 = Usuario(nombre, apellido, correo)
 usuario1.validar_correo("@utb.edu.co")
+
+class Profesor(MostrarInfo):
+    def __init__(self, nombre, apellido, materia):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.materia = materia
+
+    print("\n--- Lista de profesores ---\n")
+
+    def mostrar_informacion(self):
+        print(f"    {self.nombre} {self.apellido}")
+
+profesor1 = Profesor("Yuranis", "Henriquez", "POO")
+profesor2 = Profesor("Eder", "Barrios", "Cálculo Diferencial")
+profesor3 = Profesor("Kelly", "Tarrá", "Inglés IV")
+
+
+profesor1.mostrar_informacion()
+profesor2.mostrar_informacion()
+profesor3.mostrar_informacion()
