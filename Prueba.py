75d7dce21c48a10c4704ea7592bf19490f81295c
Ivan Banda <ivanbandaf27@outlook.com>
2026-05-09 15:36:17 -0500

    funcion menu_profesor

--- Prueba.py
+++ Prueba.py
@@ -30,14 +30,12 @@ class Usuario(MostrarInfo): #Permitir a los estudiantes acceder con su correo in
         print(f"\n----- Bienvenido/a {self.nombre} -----\n")
         print(f"\nUTBRS                        | Mis Reseñas | {self.correo} |\n")
 
-
 nombre = input("Ingrese su nombre: ")
 apellido = input("Ingrese su apellido: ")
 correo = input("Ingrese su correo: ")
 
 usuario1 = Usuario(nombre, apellido, correo)
 usuario1.validar_correo("@utb.edu.co")
-
 class Profesor(MostrarInfo):
     def __init__(self, nombre):
         self.nombre = nombre
@@ -45,14 +43,14 @@ class Profesor(MostrarInfo):
     print("\n--- Lista de profesores ---\n")
 
     def mostrar_informacion(self):
-        print(f" -- {self.nombre}")
+        print(f"  --  {self.nombre}")
 
-profesor1 = Profesor("profesor 1")
-profesor2 = Profesor("profesor 2")
-profesor3 = Profesor("profesor 3")
-profesor4 = Profesor("profesor 4")
-profesor5 = Profesor("profesor 5")
-profesor6 = Profesor("profesor 6")
+profesor1 = Profesor("Profesor 1")
+profesor2 = Profesor("Profesor 2")
+profesor3 = Profesor("Profesor 3")
+profesor4 = Profesor("Profesor 4")
+profesor5 = Profesor("Profesor 5")
+profesor6 = Profesor("Profesor 6")
 
 profesor1.mostrar_informacion()
 profesor2.mostrar_informacion()
@@ -61,34 +59,45 @@ profesor4.mostrar_informacion()
 profesor5.mostrar_informacion()
 profesor6.mostrar_informacion()
 
-seleccion_profe = int(input("\n Seleccione un profesor (1-6): "))
-
 def menu_profesor(profesor):
     print(f"\nUTBRS                        | Mis Reseñas | {usuario1.correo} |\n")
     print(f"\nHas seleccionado: {profesor.nombre}\n")
-    print("\n1. Ver reseñas")
-    print("2. Publicar reseñas")
-    opcion = int(input("\n Selecciona una opción: "))
 
-    if opcion == 1:
-        print("Ver reseñas")
-    elif opcion == 2:
-        print("Publicar reseñas")
+    while True:
+        print("\n1. Ver reseñas")
+        print("2. Publicar reseñas")
+        opcion = int(input("\n Selecciona una opción: "))
+
+        if opcion == 1:
+            print("Ver reseñas")
+            break
+        elif opcion == 2:
+            print("Publicar reseñas")
+            break
+        else:
+            print("\nOpción inválida")
+
+
+while True:
+    seleccion_profe = int(input("\nSeleccione un profesor (1-6): "))
+
+    if seleccion_profe == 1:
+        menu_profesor(profesor1)
+        break
+    elif seleccion_profe == 2:
+        menu_profesor(profesor2)
+        break
+    elif seleccion_profe == 3:
+        menu_profesor(profesor3)
+        break
+    elif seleccion_profe == 4:
+        menu_profesor(profesor4)
+        break
+    elif seleccion_profe == 5:
+        menu_profesor(profesor5)
+        break
+    elif seleccion_profe == 6:
+        menu_profesor(profesor6)
+        break
     else:
-        print("Opción inválida")
-
-
-if seleccion_profe == 1:
-    menu_profesor(profesor1)
-elif seleccion_profe == 2:
-    menu_profesor(profesor2)
-elif seleccion_profe == 3:
-    menu_profesor(profesor3)
-elif seleccion_profe == 4:
-    menu_profesor(profesor4)
-elif seleccion_profe == 5:
-    menu_profesor(profesor5)
-elif seleccion_profe == 6:
-    menu_profesor(profesor6)
-else:
-    print("Opción inválida")
+        print("\nOpción inválida")
